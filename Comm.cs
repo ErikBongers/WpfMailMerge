fc1b5a0166dbfdf6857ab550b934ec8af6401e1e
Erik Bongers <mail@erikbongers.net>
2026-03-23 19:32:15 +0100

    Create machine name to clipboard function.

---
+++ Comm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMailMerge
+    {
+    internal class Comm
+        {
+
+        public static void PutMachineNameOnClipboard()
+            {
+            var machine = System.Environment.MachineName;
+            if (machine == null)
+                return;
+            var user = System.Environment.UserName;
+            if (user == null)
+                return;
+            System.Windows.Clipboard.SetText($"DKO3PLUGIN:{machine}.{user}");
+            }
+        }
+    }
