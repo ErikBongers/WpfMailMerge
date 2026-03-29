471046baabdd817114f9a4b1d894c157d87f4297
Erik Bongers <mail@erikbongers.net>
2026-03-29 19:43:54 +0200

    Formatting.

--- Cursor.cs
+++ Cursor.cs
@@ -7,17 +7,14 @@ using System.Windows.Input;
 
 namespace WpfMailMerge;
 
-internal class Cursor<T>
+internal class Cursor<T>(IEnumerator<T> enumerator)
     {
-    private readonly IEnumerator<T> enumerator;
-    public Cursor(IEnumerator<T> enumerator) {
-        this.enumerator = enumerator;
-        }
+    private readonly IEnumerator<T> enumerator = enumerator;
 
     public void Eat() { this.MoveNext(); }
     public bool MoveNext() { return this.enumerator.MoveNext(); }
     public T Current { get { return this.enumerator.Current; } }
-    public bool EOF() { return this.enumerator.Current is null; }
+    public bool EOF() { return this.enumerator.Current is null; } //todo: use this function!
 
     public void Skip(Func<T, bool> until)
         {
